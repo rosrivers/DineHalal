3c1d9a155eaa8543d1b7bf2c78b8413252ae1afa
Joana2337 <joana.osei37@myhunter.cuny.edu>
2025-03-05 09:20:18 -0500

    Initial Commit

---
+++ Dine Halal/Dine_HalalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dine_HalalApp.swift
+//  Dine Halal
+//
+//  Created by Joanne on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Dine_HalalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
