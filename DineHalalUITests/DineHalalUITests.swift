f574e5143ae562f5a8eb55a9c9cb524a11850f27
Rosa Rivera <riverasoto.rosa@gmail.com>
2025-03-19 17:39:54 -0400

    .

--- DineHalalUITests/DineHalalUITests.swift
+++ DineHalalUITests/DineHalalUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class Dine_HalalUITests: XCTestCase {
+final class DineHalalUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
